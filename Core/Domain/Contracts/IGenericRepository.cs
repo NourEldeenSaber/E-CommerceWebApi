c0da824059f088415da85a5bceb793ff7c0efad7
NourSaber <nour01saber@gmail.com>
2026-04-25 17:11:12 +0300

    feat(repository): add IGenericReopsitory And GenericReopsitory

---
+++ Core/Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+
+namespace Domain.Contracts
+{
+    public interface IGenericRepository<TEntity,TKey> where TEntity : BaseEntity<TKey>
+    {
+        //GetAll
+        Task<IEnumerable<TEntity>> GetAllAsync(bool asNoTracking = false);
+        //GetById
+        Task<TEntity?> GetByIdAsync(TKey id);
+        //Add
+        Task AddAsync(TEntity entity);
+        //Remove
+        void Delete(TEntity entity);
+        //Update
+        void Update(TEntity entity);
+    }
+}
---
+++ Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Contracts;
+using Domain.Entities;
+using Presistence.Data;
+
+namespace Presistence.Repositories
+{
+    public class GenericRepository<TEntity,TKey> 
+        : IGenericRepository<TEntity,TKey> where TEntity : BaseEntity<TKey>
+    {
+        #region Constructor
+        
+        private readonly StoreDbContext _dbContext;
+        public GenericRepository(StoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        } 
+        
+        #endregion
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool asNoTracking = false)
+        => asNoTracking ? await _dbContext.Set<TEntity>().AsNoTracking().ToListAsync() 
+            : await _dbContext.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity?> GetByIdAsync(TKey id)
+        => await _dbContext.Set<TEntity>().FindAsync(id);
+
+        public async Task AddAsync(TEntity entity)
+        => await _dbContext.Set<TEntity>().AddAsync(entity);
+
+        public void Update(TEntity entity)
+        => _dbContext.Set<TEntity>().Update(entity);
+
+        public void Delete(TEntity entity)
+        => _dbContext.Set<TEntity>().Remove(entity);
+
+    }
+}
