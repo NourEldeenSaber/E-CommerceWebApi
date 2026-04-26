a81ee4819ba9c8ba933b65ca8bccccaa6e720250
NourSaber <nour01saber@gmail.com>
2026-04-26 03:03:30 +0300

    feat(productController): Add 4 Actions in controller

---
+++ Infrastructure/Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sevices.Abstraction.Contracts;
+using Shared.Dtos;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public ProductsController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        //GetAllProducts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductResultDto>>> GetAllProductsAsync()
+            => Ok(await _serviceManager.ProductService.GetAllProductsAsync());
+
+        //GetAllBrands
+        [HttpGet("Brands")] //BaseUrl/Products/Brands
+        public async Task<ActionResult<IEnumerable<BrandResultDto>>> GetAllBrandsAsync()
+            => Ok(await _serviceManager.ProductService.GetAllBrandsAsync());
+
+        //GetAllTypes 
+        [HttpGet("Types")]//BaseUrl/Products/Types
+        public async Task<ActionResult<IEnumerable<TypeResultDto>>> GetAllTypesAsync()
+            => Ok(await _serviceManager.ProductService.GetAllTypesAsync());
+
+        //GetProductById
+        [HttpGet("{id:int}")]//BaseUrl/Products/10
+        public async Task<ActionResult<ProductResultDto>> GetProductByIdAsync(int id)
+            => Ok( await _serviceManager.ProductService.GetProductByIdAsync(id));
+        
+    }
+}
