6f82656e0add94a4f609524f79a2d85470a24ea7
NourSaber <nour01saber@gmail.com>
2026-05-03 17:00:25 +0300

    feat(create-jwt-token):Generates a JWT (JSON Web Token) containing user information and claims, used to authenticate users securely in subsequent requests.

--- Core/Services/Implementations/AuthenticationService.cs
+++ Core/Services/Implementations/AuthenticationService.cs
@@ -1,8 +1,12 @@
 ﻿using Domain.Entities.IdentityModule;
 using Domain.Exceptions;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using Sevices.Abstraction.Contracts;
 using Shared.Dtos.IdentityModule;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Services.Implementations
 {
@@ -10,7 +14,7 @@ namespace Services.Implementations
     {
         private readonly UserManager<User> _userManager;
 
-        public AuthenticationService(UserManager<User> userManager)
+        public AuthenticationService(UserManager<User> userManager )
         {
             _userManager = userManager;
         }
@@ -26,7 +30,9 @@ namespace Services.Implementations
             if (!result) 
                 throw new UnAuthorizedException();
 
-            return new UserResultDto(user.DisplayName, "Token", user.Email);
+            var token = await CreateTokenAsync(user);
+
+            return new UserResultDto(user.DisplayName, token, user.Email!);
         }
 
         public async Task<UserResultDto> RegisterAsync(RegisterDto registerDto)
@@ -48,7 +54,45 @@ namespace Services.Implementations
                 throw new ValidationException(errors);
             }
 
-            return new UserResultDto(user.DisplayName, "Token", user.Email);
+            var token = await CreateTokenAsync(user);
+
+            return new UserResultDto(user.DisplayName, token, user.Email);
         }
+
+
+        #region Helper Methods
+
+        //Generate Token
+        private async Task<string> CreateTokenAsync(User user)
+        {
+            // Claims [Name, Email,Rloe]
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name , user.DisplayName!),
+                new Claim(ClaimTypes.Email , user.Email!),
+            };
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles) 
+                claims.Add(new Claim(ClaimTypes.Role,role));
+
+            //secret key => SymmetricSecurityKey
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("74312bc9f0d59ec912574abebd6fbb8cca7f2e7d91764123296a5b6b5fa02a64"));
+
+            //Algorithm
+            var signInCreds = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "https://localhost:7199/",
+                audience: "AngularProject",
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(10),
+                signingCredentials: signInCreds
+            );
+
+            //JWTSecurityHandler => WriteToken
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        #endregion
     }
 }
