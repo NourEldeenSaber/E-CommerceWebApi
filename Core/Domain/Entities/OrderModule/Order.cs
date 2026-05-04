ce268b7f85754b83801f68146c385d0a802221d6
NourSaber <nour01saber@gmail.com>
2026-05-04 21:22:24 +0300

    refactor(order-module): add constructor to entities in order module

--- Core/Domain/Entities/OrderModule/Order.cs
+++ Core/Domain/Entities/OrderModule/Order.cs
@@ -4,6 +4,19 @@ namespace Domain.Entities.OrderModule
 {
     public class Order : BaseEntity<Guid>
     {
+        public Order()
+        {
+            
+        }
+        public Order(string userEmail, ShippingAddress shippingAddress, ICollection<OrderItem> orderItems, DeliveryMethod deliveryMethod, decimal subTotal)
+        {
+            UserEmail = userEmail;
+            ShippingAddress = shippingAddress;
+            OrderItems = orderItems;
+            DeliveryMethod = deliveryMethod;
+            SubTotal = subTotal;
+        }
+
         public string UserEmail { get; set; } = string.Empty;
         public ShippingAddress ShippingAddress { get; set; }
         public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
