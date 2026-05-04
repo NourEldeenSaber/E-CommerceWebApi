2d40aa9015036cf34e98fdf183426f6fe995e68c
NourSaber <nour01saber@gmail.com>
2026-05-05 02:30:59 +0300

    feat(order-module): add contract of Order service [IOrderService] and create profiles for mapping

---
+++ Core/Services/MappingProfiles/OrderProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Domain.Entities.OrderModule;
+using Shared.Dtos.OrderModule;
+
+namespace Services.MappingProfiles
+{
+    internal class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Address, AddressDto>().ReverseMap();
+            CreateMap<DeliveryMethod, DeliveryMethodResult>();
+            CreateMap<OrderItem , OrderItemDto>()
+                .ForMember(des => des.ProductId , opt => opt.MapFrom(src => src.Product.ProductId))
+                .ForMember(des => des.ProductName , opt => opt.MapFrom(src => src.Product.ProductName))
+                .ForMember(des => des.PictureUrl , opt => opt.MapFrom(src => src.Product.PictureUrl));
+            CreateMap<Order, OrderResult>()
+                .ForMember(des => des.PaymentStatus, opt => opt.MapFrom(src => src.PaymentStatus.ToString()))
+                .ForMember(des => des.DeliveryMethod, opt => opt.MapFrom(src => src.DeliveryMethod.ShortName))
+                .ForMember(des => des.Total, opt => opt.MapFrom(src => src.SubTotal + src.DeliveryMethod.Price));
+        }
+    }
+}
--- Core/Sevices.Abstraction/Contracts/IOrderService.cs
+++ Core/Sevices.Abstraction/Contracts/IOrderService.cs
@@ -1,10 +1,16 @@
-﻿namespace Sevices.Abstraction.Contracts
+﻿using Shared.Dtos.OrderModule;
+
+namespace Sevices.Abstraction.Contracts
 {
     public interface IOrderService
     {
         // GetById =>> Take Id [Guid] ==> return OrderResultDto
-        // GetAllByEmial =>> Take Email [string] ==> return IEnumerable<OrderResultDto>
+        Task<OrderResult> GetOrderByIdAsync(Guid id);
+        // GetAllByEmail =>> Take Email [string] ==> return IEnumerable<OrderResultDto>
+        Task<IEnumerable<OrderResult>> GetOrdersByEmailAsync(string userEmail);
         // CreateOrder =>> Take OrderRequest, Email [string] ==> return OrderResultDto
+        Task<OrderResult> CreateOrderAsync(OrderRequest orderRequest , string userEmail);
         // GetDelivaryMethods =>  return IEnumerable<DeliveryMethodsResultDto>
+        Task<IEnumerable<DeliveryMethodResult>> GetDeliveryMethodsAsync();
     }
 }
